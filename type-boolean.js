b1876ea0a96480df3eaa5fd930ef9b6e341553d1
AllanRanier <allanranier18@gmail.com>
2022-12-07 14:57:06 -0300

    first commit

---
+++ type-boolean.js
@@ -0,0 +1,27 @@
+const num1 = 5;
+const num2 = 9;
+
+console.log(num1 === num2)
+
+const num3 = 9;
+const num4 = 9;
+
+console.log(num3 === num4);
+
+// diferença entre - let e const
+//cadastroAtivado = true;
+
+
+const cidade = "Belo Horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const cidade1 = "belo horizonte";
+const input1 = "Belo Horizonte";
+const input1Minusculo = input.toLowerCase();
+
+console.log(cidade1 === input1Minusculo);
+
+const senha1 = "minhasenhae15963";
+console.log(senha1.length);
---
+++ type-string.js
@@ -0,0 +1,20 @@
+const texto1 = "olá mundo!";
+const texto2 = 'olá mundo!';
+const senha = "senhaSuperSegura159";
+const StringDeNumeros = "15963";
+
+/*const citacao = "A lari disse 'eae!'";
+console.log(citacao)*/
+    
+//concatenar (+)
+
+const meuNome = "Larissa";
+const citacao1 = "Meu nome é ";
+console.log(citacao1 + meuNome)
+
+/*const cifrao = '/u0024';
+console.log(cifrao)*/
+
+
+
+
